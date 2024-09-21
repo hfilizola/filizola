fd746dcacfaa72cf795e0537065d01185955db34
hfilizola <hectorfg44@hotmail.com>
2024-09-20 18:28:47 -0600

    Add files via upload

---
+++ Jupyter_Notebook.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center>
+#     <img src="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0105EN-SkillsNetwork/labs/Module2/images/SN_web_lightmode.png" width="300" alt="cognitiveclass.ai logo">
+# </center>
+# 
+
+# #### Add your code below following the instructions given in the course
+# 
+
+# # Data Science Notebook
+
+# ## Introduction
+# This notebook covers various aspects of data science, including languages, libraries, tools, and examples of arithmetic expressions.
+
+# ## Data Science Languages
+# - Python
+# - R
+# - SQL
+# - Java
+# - Julia
+
+# ## Data Science Libraries
+# - Pandas
+# - NumPy
+# - Matplotlib
+# - Scikit-learn
+# - TensorFlow
+
+# ## Data Science Tools
+# 
+# | Tool             | Description                                                |
+# |------------------|------------------------------------------------------------|
+# | Jupyter Notebook | Open-source web application for creating and sharing code documents |
+# | RStudio          | IDE for R programming                                      |
+# | Apache Spark     | Cluster computing system for big data processing            |
+# | Tableau          | Data visualization tool                                     |
+
+# ## Arithmetic Expression Examples
+# Below are examples of basic arithmetic expressions:
+
+# In[5]:
+
+
+# Multiply and add numbers
+a = 5
+b = 10
+result = (a * b) + b
+result
+
+
+# In[6]:
+
+
+# Convert minutes to hours
+minutes = 120
+hours = minutes / 60
+hours
+
+
+# ## Objectives
+# - Learn about Data Science languages and libraries
+# - Explore common Data Science tools
+# - Perform arithmetic operations
+# - Convert time units
+# 
+
+# ## Author
+# Hector Renato Filizola
+# 
